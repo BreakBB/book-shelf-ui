690df0752a3515a00f118566f942a4fa1c2cfa80
BreakBB <b.boeing@online.de>
2021-12-17 10:10:20 +0100

    Improve useBooks tests

--- src/hooks/useBooks.spec.ts
+++ src/hooks/useBooks.spec.ts
@@ -1,7 +1,7 @@
 import {renderHook} from '@testing-library/react-hooks';
 import useBooks from './useBooks';
 import {act} from '@testing-library/react';
-import {createNewBook, getAllBooks} from '../bookService';
+import {createNewBook, deleteBook as remoteDeleteBook, getAllBooks} from '../bookService';
 import {TEST_BOOKS} from '../testUtils';
 
 jest.mock('../bookService');
@@ -9,6 +9,14 @@ jest.mock('../bookService');
 describe('useBooks', () => {
     const getAllBooksMock = getAllBooks as jest.Mock;
     const createNewBookMock = createNewBook as jest.Mock;
+    const remoteDeleteBookMock = remoteDeleteBook as jest.Mock;
+
+    const onSuccessMock = jest.fn();
+    const onErrorMock = jest.fn();
+
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
 
     it('should initially return no books', () => {
         const {result} = renderHook(() => useBooks());
@@ -16,48 +24,97 @@ describe('useBooks', () => {
         expect(result.current.books.length).toBe(0);
     });
 
-    it('should fetch and return books', async () => {
-        getAllBooksMock.mockReturnValue([TEST_BOOKS.harryPotter1]);
+    describe('fetchBooks', () => {
+        it('should fetch and return books', async () => {
+            getAllBooksMock.mockReturnValue([TEST_BOOKS.harryPotter1]);
 
-        const {result} = renderHook(() => useBooks());
-        await act(() => result.current.fetchBooks());
+            const {result} = renderHook(() => useBooks());
+            await act(() => result.current.fetchBooks());
 
-        expect(result.current.books.length).toBe(1);
-        expect(result.current.books[0]).toBe(TEST_BOOKS.harryPotter1);
+            expect(getAllBooksMock).toHaveBeenCalled();
+            expect(result.current.books.length).toBe(1);
+            expect(result.current.books[0]).toBe(TEST_BOOKS.harryPotter1);
+        });
+
+        it('should handle errors', async () => {
+            getAllBooksMock.mockImplementation(() => {
+                throw new Error();
+            });
+
+            const {result} = renderHook(() => useBooks());
+            await act(() => result.current.fetchBooks());
+
+            expect(getAllBooksMock).toHaveBeenCalled();
+            expect(result.current.books.length).toBe(0);
+        });
     });
 
-    it('should add a new book', async () => {
-        createNewBookMock.mockReturnValue(TEST_BOOKS.harryPotter1);
+    describe('addBook', () => {
+        it('should add a new book', async () => {
+            createNewBookMock.mockReturnValue(TEST_BOOKS.harryPotter1);
 
-        const {result} = renderHook(() => useBooks());
+            const {result} = renderHook(() => useBooks());
 
-        expect(result.current.books.length).toBe(0);
-        await act(() =>
-            result.current.addBook(
-                TEST_BOOKS.harryPotter1,
-                () => {},
-                () => {}
-            )
-        );
-        expect(result.current.books.length).toBe(1);
-        expect(result.current.books[0]).toBe(TEST_BOOKS.harryPotter1);
+            expect(result.current.books.length).toBe(0);
+
+            await act(() => result.current.addBook(TEST_BOOKS.harryPotter1, onSuccessMock, onErrorMock));
+
+            expect(createNewBookMock).toHaveBeenCalled();
+            expect(onSuccessMock).toHaveBeenCalled();
+            expect(onErrorMock).not.toHaveBeenCalled();
+            expect(result.current.books.length).toBe(1);
+            expect(result.current.books[0]).toBe(TEST_BOOKS.harryPotter1);
+        });
+
+        it('should call onError handler', async () => {
+            createNewBookMock.mockImplementation(() => {
+                throw new Error();
+            });
+
+            const {result} = renderHook(() => useBooks());
+
+            expect(result.current.books.length).toBe(0);
+
+            await act(() => result.current.addBook(TEST_BOOKS.harryPotter1, onSuccessMock, onErrorMock));
+
+            expect(createNewBookMock).toHaveBeenCalled();
+            expect(onSuccessMock).not.toHaveBeenCalled();
+            expect(onErrorMock).toHaveBeenCalled();
+            expect(result.current.books.length).toBe(0);
+        });
     });
 
-    it('should delete a book', async () => {
-        getAllBooksMock.mockReturnValue([TEST_BOOKS.harryPotter1, TEST_BOOKS.harryPotter2]);
+    describe('deleteBook', () => {
+        it('should delete a book', async () => {
+            getAllBooksMock.mockReturnValue([TEST_BOOKS.harryPotter1, TEST_BOOKS.harryPotter2]);
 
-        const {result} = renderHook(() => useBooks());
-        await act(() => result.current.fetchBooks());
+            const {result} = renderHook(() => useBooks());
+            await act(() => result.current.fetchBooks());
 
-        expect(result.current.books.length).toBe(2);
-        await act(() =>
-            result.current.deleteBook(
-                TEST_BOOKS.harryPotter1.isbn,
-                () => {},
-                () => {}
-            )
-        );
-        expect(result.current.books.length).toBe(1);
-        expect(result.current.books[0]).toBe(TEST_BOOKS.harryPotter2);
+            expect(result.current.books.length).toBe(2);
+            await act(() => result.current.deleteBook(TEST_BOOKS.harryPotter1.isbn, onSuccessMock, onErrorMock));
+
+            expect(remoteDeleteBookMock).toHaveBeenLastCalledWith(TEST_BOOKS.harryPotter1.isbn);
+            expect(onSuccessMock).toHaveBeenCalled();
+            expect(onErrorMock).not.toHaveBeenCalled();
+            expect(result.current.books.length).toBe(1);
+            expect(result.current.books[0]).toBe(TEST_BOOKS.harryPotter2);
+        });
+
+        it('should call onError handler', async () => {
+            getAllBooksMock.mockReturnValueOnce([TEST_BOOKS.harryPotter1, TEST_BOOKS.harryPotter2]);
+            remoteDeleteBookMock.mockImplementationOnce(() => {
+                throw new Error();
+            });
+
+            const {result} = renderHook(() => useBooks());
+            await act(() => result.current.fetchBooks());
+            await act(() => result.current.deleteBook(TEST_BOOKS.harryPotter1.isbn, onSuccessMock, onErrorMock));
+
+            expect(remoteDeleteBookMock).toHaveBeenLastCalledWith(TEST_BOOKS.harryPotter1.isbn);
+            expect(onSuccessMock).not.toHaveBeenCalled();
+            expect(onErrorMock).toHaveBeenCalled();
+            expect(result.current.books.length).toBe(2);
+        });
     });
 });
